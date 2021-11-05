c54f7d883efcd4645ea29a42e7f2526cc097668e
Cédric Marcelly <cmarcelly@agencearobas.ca>
2021-11-05 10:23:08 -0400

    prevent LICENSE.txt file in build prod scripts folder

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -42,6 +42,8 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin({
             cleanOnceBeforeBuildPatterns: ['**/*'],
+            protectWebpackAssets: false,
+            cleanAfterEveryBuildPatterns: ['**/*.LICENSE.txt'],
         }),
         new CopyWebpackPlugin({
             patterns: [
