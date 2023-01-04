8a77de9eda1b7c41fb440cedcbe119ba2feb7a07
Cédric Marcelly <cedric.marcelly@cossette.com>
2023-01-04 17:02:33 -0500

    wip

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,9 @@ module.exports = {
     plugins: {
         'postcss-preset-env': {
             browsers: 'last 2 versions',
+            'features': {
+                'custom-properties': false
+            }
         },
     },
 }
